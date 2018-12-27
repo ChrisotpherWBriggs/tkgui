5f6da8e9a8d5453d61f2e05a7034b84b4a7d6e85
chris <cbriggs@mcoa.com>
2018-12-27 16:35:29 -0500

    working on date searches

--- filetest.py
+++ filetest.py
@@ -1,16 +1,42 @@
 import os
 import csv
+import re
 os.chdir("C:\Users\CBriggs\Desktop\Python Scripts")
 
-s1= raw_input()
-s2 = raw_input()
-s3 = raw_input()
+s1= raw_input("Enter Last Name")
+s2 = raw_input("Enter First Name")
+s3 = raw_input("Enter Group Number")
+s4 = raw_input("Search Claims From Date (mm/dd/yyyy")
+s5 = raw_input("Search Claims Through Date (mm/dd/yyyy")
 
+
+pattern = re.compile("[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]")
+
+fDate = s4.split("/")
+fDate2 = (fDate[2] + fDate[0] + fDate[1])
+
+tDate = s5.split("/")
+tDate2 = (tDate[2] + tDate[0] + tDate[1])
+
+if re.match(pattern, s4):
+    print(s4.split("/"))
+else:
+    print("no")
 csvfile = open("claims.csv", "r")
 outfile = open("1.txt", "wb")
 outfile2 = csv.writer(outfile)
 readCSV = csv.reader(csvfile)
 
+for row in readCSV:
+    z = row[4].split("/")
+    y = (z[2] + z[0] + z[1])
+    if y >= fDate2 and y <= tDate2:
+        outfile2.writerow(row)
+
+
+
+
+    """
 def claimSearch():
     if s1.upper() and s2.upper() and s3.upper():
         for row in readCSV:
@@ -44,6 +70,7 @@ def claimSearch():
         print("no terms found")
 
 claimSearch()
+"""
 csvfile.close()
 outfile.close()
 
--- rx_data.py
+++ rx_data.py
@@ -4,7 +4,7 @@ import csv
 import os
 
 
-
+#Search function
 
 def claimSearch():
     os.chdir("C:\Users\CBriggs\Desktop\Python Scripts")
@@ -96,11 +96,16 @@ frame2 = Frame(root)
 label = Label(frame2, text="Enter Last Name:")
 label1 = Label(frame2, text="Enter First Name: ")
 label2 = Label(frame2, text="Enter Group ID:")
+label3 = Label(frame2, text="Enter Beginning Date Range (mm/dd/yyyy)")
+label4 = Label(frame2, text="Enter Beginning Date Range (mm/dd/yyyy)")
 
+label.configure()
 #entry boxes
 search = Entry(frame2)
 search1 = Entry(frame2)
 search2 = Entry(frame2)
+search3 = Entry(frame2)
+search4 = Entry(frame2)
 
 #button
 b1 = Button(frame2, text="Search", command=claimSearch)
@@ -109,13 +114,17 @@ b1 = Button(frame2, text="Search", command=claimSearch)
 canvas.grid(row=0, sticky=W)
 frame.grid(row=0, pady=25, sticky=W)
 frame2.grid(row=1, pady=25, sticky=W)
-label.grid(row=1, column=0)
-search.grid(row=1, column=1)
-label1.grid(row=2, column=0)
-search1.grid(row=2, column=1)
-label2.grid(row=3, column=0)
-search2.grid(row=3, column=1)
-b1.grid(row=4, columnspan=3)
+label.grid(row=1, column=0, padx=20)
+search.grid(row=1, column=1, padx=20)
+label1.grid(row=2, column=0, padx=20)
+search1.grid(row=2, column=1, padx=20)
+label2.grid(row=3, column=0, padx=20)
+search2.grid(row=3, column=1, padx=20)
+label3.grid(row=4, column=0, padx=20)
+search3.grid(row=4, column=1, padx=20)
+label4.grid(row=5, column=0, padx=20)
+search4.grid(row=5, column=1, padx=20)
+b1.grid(row=6, columnspan=3, pady=30)
 
 
 
